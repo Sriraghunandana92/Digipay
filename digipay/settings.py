9e40fbbebcf2f0c2d721756c81cde1257bb580c4
Sriraghunandana92 <raghunandanakeremane@gmail.com>
2023-11-02 19:58:59 +0530

    updated-6

--- digipay/settings.py
+++ digipay/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6r=w^_!eb%8vfio*g5mg+tw%z+x693o*3+i5vx9e%gpsz=ckfy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['digipay-five.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 
